d574572d9b5443ef55b64884b7ed9459818cf237
Peter Dye <peterdye@Peters-MacBook-Pro.local>
2020-03-25 23:26:39 -0400

    more work on post()

--- functions.py
+++ functions.py
@@ -5,6 +5,17 @@ def post(userID):
     if len(content) > 1000:
         print("That post was too long! Maximum length is 1000 characters.")
 
+    GroupID = input("What group do you want to post in? (Hit enter for none) GroupID: ")
+    if len(groupID) = 0:
+        groupID = "NULL"
 
+    TopicID = input("What is the topic of your post? (Hit enter for none) TopicID: ")
+    if len(TopicID) = 0:
+        TopicID = "NULL"
+
+    # Check what parent post is in the doc
 
     return 0
+
+
+post(peterdye1)
